7dbaace71381a101db24b7e808a7b419cee90d54
Tomas <tomasurielmp@gmail.com>
2022-01-22 11:27:49 -0300

    Initial Commit

---
+++ src/main/java/com/utn/frlp/tecle/TecleApplication.java
@@ -0,0 +1,13 @@
+package com.utn.frlp.tecle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utn/frlp/tecle/TecleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.frlp.tecle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
